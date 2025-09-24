09f68e551ea997cc53920438c1fbbe6c19ba387f
neptumiun <tymothe.billerey@gmail.com>
2025-09-24 13:25:33 +0200

    add  71a

---
+++ 71/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+  int
+main(int argc, char *argv[])
+{
+  int n;
+  std::cin >> n;
+  std::string s;
+  while (n--)
+  {
+    std::cin >> s;
+    if (s.size() < 11)
+    {
+      std::cout << s << std::endl;
+      continue;
+    }
+    std::cout << s[0] << s.size() - 2 << s[s.size() - 1] << std::endl;
+  }
+
+  return 0;
+}
