fdf9fa78b52ef3d1669db56dd1bfae44e8981f1e
neptumiun <tymothe.billerey@gmail.com>
2025-09-24 10:17:59 +0200

    style: clang-format

--- 158/b.cpp
+++ 158/b.cpp
@@ -13,24 +13,24 @@ main(int argc, char *argv[])
     std::cin >> k;
     switch (k)
     {
-    case 1:
-      a++;
-      break;
-    case 2:
-      b++;
-      break;
-    case 3:
-      c++;
-      break;
-    case 4:
-      d++;
-      break;
+      case 1:
+        a++;
+        break;
+      case 2:
+        b++;
+        break;
+      case 3:
+        c++;
+        break;
+      case 4:
+        d++;
+        break;
     }
   }
   int t = d;
-  t+= b/2;
+  t += b / 2;
   b = b % 2;
-  if (a>c)
+  if (a > c)
   {
     t += c;
     a -= c;
@@ -38,18 +38,18 @@ main(int argc, char *argv[])
   }
   else
   {
-    t+=c;
-    a=0;
+    t += c;
+    a = 0;
   }
   if (b)
   {
     t++;
     a -= 2;
   }
-  if (a>0)
+  if (a > 0)
   {
-    t+= a/4;
-    t+= (a%4) ? 1 : 0;
+    t += a / 4;
+    t += (a % 4) ? 1 : 0;
   }
   std::cout << t << std::endl;
   return 0;
