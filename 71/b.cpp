bf90362995108b554dfa3649d479196e2145992b
neptumiun <tymothe.billerey@gmail.com>
2025-09-24 13:25:47 +0200

    add 71b

---
+++ 71/b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+  int n, k, t;
+  std::cin >> n >> k >> t;
+  int full = n * t / 100;
+  int part = (n * t - 100 * full) * k / 100;
+  for (int i = 0; i < full; ++i)
+    std::cout << k << " ";
+  if (full < n)
+  {
+    std::cout << part << " ";
+    for (int i = 1; i < n - full; ++i)
+      std::cout << "0 ";
+  }
+  return 0;
+}
